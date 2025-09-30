633695ffa16e307faa692d3c51d99431dc794037
Rob Dickerson <rob@robd.io>
2025-09-30 10:22:23 -0500

    Setup of Lab3.

--- src/main/java/edu/augustana/csc305/lab3/RegistrationController.java
+++ src/main/java/edu/augustana/csc305/lab3/RegistrationController.java
@@ -2,6 +2,7 @@ package edu.augustana.csc305.lab3;
 
 public class RegistrationController {
 
+    private final RegistrationModel model = new RegistrationModel();
     private final RegistrationView view = new RegistrationView();
 
     public RegistrationView getView() {
